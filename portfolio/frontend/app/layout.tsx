f091bed5d1e428b500928f2b5d4dafaf5c5fe158
Sebastian Hilger <s.hilger@hm.edu>
2026-01-16 21:54:27 +0100

    Fix hydration warning in layout

--- portfolio/frontend/app/layout.tsx
+++ portfolio/frontend/app/layout.tsx
@@ -16,16 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
           href="https://fonts.googleapis.com/css2?family=Crimson+Text:wght@400;600;700&family=Oxygen:wght@300;400;700&display=swap"
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }

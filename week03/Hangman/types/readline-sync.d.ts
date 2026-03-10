e4c278bd7679af5ea9837e747062934063aca865
Sebastian Hilger <s.hilger@hm.edu>
2026-03-10 15:44:12 +0100

    Delete week03 directory

--- week03/Hangman/types/readline-sync.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'readline-sync' {
-  // Minimal typing for the question() API used in this project.
-  function question(prompt?: string): string;
-  const readlineSync: { question(prompt?: string): string };
-  export = readlineSync;
-}
--- week03/hangman2/types/readline-sync.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'readline-sync' {
-  function question(prompt?: string): string;
-  const readlineSync: { question(prompt?: string): string };
-  export = readlineSync;
-}
